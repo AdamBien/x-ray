e42c28b7643e44bd92e3d0374e5813e48251543c
Adam Bien <abien@adam-bien.com>
2012-04-01 07:31:48 +0200

    RefererScavengerTimer exposed as Singleton

--- x-ray-services/src/main/java/com/abien/xray/business/scavenger/boundary/RefererScavengerTimer.java
+++ x-ray-services/src/main/java/com/abien/xray/business/scavenger/boundary/RefererScavengerTimer.java
@@ -1,18 +1,27 @@
 package com.abien.xray.business.scavenger.boundary;
 
+import com.abien.xray.business.monitoring.PerformanceAuditor;
 import com.abien.xray.business.scavenger.control.RefererScavenger;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.ejb.DependsOn;
 import javax.ejb.ScheduleExpression;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import javax.ejb.Timeout;
 import javax.ejb.TimerConfig;
 import javax.ejb.TimerService;
 import javax.inject.Inject;
+import javax.interceptor.Interceptors;
 
 /**
  *
  * @author adam bien, adam-bien.com
  */
+@Singleton
+@Startup
+@DependsOn("Configuration")
+@Interceptors(PerformanceAuditor.class)
 public class RefererScavengerTimer {
 
     @Inject
