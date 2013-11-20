d7471cc70a88caacdfbd72e679aad14fa5abae45
Adam Bien <abien@adam-bien.com>
2013-11-20 09:28:49 +0100

    RESTConfiguration moved to top level

---
+++ x-ray-services/src/main/java/com/abien/xray/business/RESTConfiguration.java
@@ -0,0 +1,5 @@
+
+package com.abien.xray.business;
+@javax.ws.rs.ApplicationPath("resources")
+public class RESTConfiguration extends javax.ws.rs.core.Application {
+}
--- x-ray-services/src/main/java/com/abien/xray/business/store/boundary/ApplicationConfig.java
+++
@@ -1,5 +0,0 @@
-
-package com.abien.xray.business.store.boundary;
-@javax.ws.rs.ApplicationPath("resources")
-public class ApplicationConfig extends javax.ws.rs.core.Application {
-}
