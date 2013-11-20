b47d6ce8499fef6bb0d8d1d797602e6913e74df2
Adam Bien <abien@adam-bien.com>
2013-11-20 09:29:08 +0100

    Build script provided

--- x-ray-services/deploy.sh
+++ x-ray-services/deploy.sh
@@ -3,3 +3,4 @@ JAVA_HOME=$JAVA_8_HOME
 mvn clean install
 HOST=${1:-localhost}
 "$GLASSFISH_HOME"/bin/asadmin --host $HOST --port 5348 deploy --force ./target/x-ray.war
+start http://localhost:5380/x-ray/resources/maintenance/
