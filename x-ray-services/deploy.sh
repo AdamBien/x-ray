4301975e9746ed45ccd1b2fbc2d3a0febbfbd2f7
Adam Bien <abien@adam-bien.com>
2013-12-31 06:55:40 +0100

    Versioning is working

--- x-ray-services/deploy.sh
+++ x-ray-services/deploy.sh
@@ -4,5 +4,5 @@ mvn clean install
 $WILDFLY_HOME/bin/jboss-cli.sh --connect shutdown
 $WILDFLY_HOME/bin/standalone.sh &
 sleep 2
-$WILDFLY_HOME/bin/jboss-cli.sh --connect --command="deploy ./target/x-ray.war"
-start http://localhost:8080/x-ray/resources/version/
+$WILDFLY_HOME/bin/jboss-cli.sh --connect --command="deploy --force ./target/x-ray.war"
+open http://localhost:8080/x-ray/resources/version/
