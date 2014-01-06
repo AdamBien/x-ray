01dcefd45a3a8ffcde1dd9b5f79aa4b682ca306f
Adam Bien <abien@adam-bien.com>
2014-01-06 06:46:21 +0100

    Undeployment included

--- x-ray-services/deploy.sh
+++ x-ray-services/deploy.sh
@@ -4,5 +4,6 @@ mvn clean install
 $WILDFLY_HOME/bin/jboss-cli.sh --connect shutdown
 $WILDFLY_HOME/bin/standalone.sh &
 sleep 2
+$WILDFLY_HOME/bin/jboss-cli.sh --connect --command="undeploy x-ray.war"
 $WILDFLY_HOME/bin/jboss-cli.sh --connect --command="deploy --force ./target/x-ray.war"
 open http://localhost:8080/x-ray/resources/version/
