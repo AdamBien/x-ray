4269c958372d2beaff7d46e010e35aa6c1c42ed5
Adam Bien <abien@adam-bien.com>
2014-01-06 07:06:33 +0100

    Test for fetching unknown stores added.

--- x-ray-services-st/src/test/java/com/abien/xray/business/store/boundary/StoresResourceIT.java
+++ x-ray-services-st/src/test/java/com/abien/xray/business/store/boundary/StoresResourceIT.java
@@ -8,8 +8,10 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.Assert;
+import static org.junit.Assert.assertThat;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -33,6 +35,15 @@ public class StoresResourceIT {
         JsonArray result = RESTSupport.convertToArrayFrom(this.tut.request(MediaType.TEXT_PLAIN).get(String.class));
         int nbrOfStores = result.size();
         Assert.assertThat(nbrOfStores, is(3));
+    }
+
+    @Test
+    public void fetchUnknownStore() {
+        Response response = this.tut.path("SHOULD-NOT-EXIST").
+                request(MediaType.TEXT_PLAIN).
+                get(Response.class);
+        int status = response.getStatus();
+        assertThat(status, is(204));
     }
 
 }
