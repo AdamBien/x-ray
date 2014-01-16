d70a66a8a027328e6641887575209171d803e46b
Adam Bien <abien@adam-bien.com>
2014-01-16 16:29:23 +0100

    store removed, deploy script updated

--- x-ray-services/deploy.sh
+++ x-ray-services/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 JAVA_HOME=$JAVA_8_HOME
 mvn clean install
+echo '----- Redeploying x-ray-services -----'
 $WILDFLY_HOME/bin/jboss-cli.sh --connect --command="undeploy x-ray.war"
 $WILDFLY_HOME/bin/jboss-cli.sh --connect --command="deploy --force ./target/x-ray.war"
-open http://localhost:8080/x-ray/resources/version/
+
+echo '----- Running integration tests -----'
+cd ../x-ray-services-st/
+./it.sh
