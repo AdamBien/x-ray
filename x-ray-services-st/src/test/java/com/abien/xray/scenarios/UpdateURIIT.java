902244734a8d77e27234755bc3e97760e9685e50
Adam Bien <abien@adam-bien.com>
2014-01-16 15:47:00 +0100

    system tests fixed

--- x-ray-services-st/src/test/java/com/abien/xray/scenarios/UpdateURIIT.java
+++ x-ray-services-st/src/test/java/com/abien/xray/scenarios/UpdateURIIT.java
@@ -25,6 +25,7 @@ public class UpdateURIIT {
     public final static String HEADER_PREFIX = "xray_";
     private static final String ROOT_TARGET = "http://localhost:8080/x-ray/resources/hits/";
     private WebTarget tut;
+    private final static long WAIT_TIME = 8 * 1000;
 
     @Before
     public void initClient() {
@@ -33,13 +34,14 @@ public class UpdateURIIT {
     }
 
     @Test
-    public void putNewValidUri() throws UnsupportedEncodingException {
+    public void putNewValidUri() throws UnsupportedEncodingException, InterruptedException {
         String userAgentHeader = HEADER_PREFIX + HttpHeaders.USER_AGENT.toLowerCase();
 
         String uri = "/entry/news" + System.currentTimeMillis();
         Response response = this.tut.request().header(HEADER_PREFIX + "referer", "twitter").header(userAgentHeader, "mozilla").put(Entity.text(uri));
         assertThat(response.getStatus(), is(204));
         String encoded = URLEncoder.encode(uri, "UTF-8");
+        Thread.sleep(WAIT_TIME);
         String numberOfHits = this.tut.path(encoded).request().get(String.class);
         assertNotNull(numberOfHits);
         long nbr = Long.parseLong(numberOfHits);
