855e22240889ee5889365143906605ac781cfc3e
Adam Bien <abien@adam-bien.com>
2014-01-20 11:26:01 +0100

    ConstraintViolationExceptionMapper added

---
+++ x-ray-satellite/src/main/java/com/airhacks/satellite/ConstraintViolationExceptionMapper.java
@@ -0,0 +1,22 @@
+package com.airhacks.satellite;
+
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/*
+ */
+/**
+ *
+ * @author adam-bien.com
+ */
+@Provider
+public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+
+    @Override
+    public Response toResponse(ConstraintViolationException exception) {
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
+}
