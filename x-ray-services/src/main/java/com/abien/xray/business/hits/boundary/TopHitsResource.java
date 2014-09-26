20a87660c172f5ef3f9c6a0bac033b219b868ece
Adam Bien <abien@adam-bien.com>
2014-09-26 04:51:51 +0200

    Collect included

--- x-ray-services/src/main/java/com/abien/xray/business/hits/boundary/TopHitsResource.java
+++ x-ray-services/src/main/java/com/abien/xray/business/hits/boundary/TopHitsResource.java
@@ -4,8 +4,10 @@ import com.abien.xray.business.monitoring.PerformanceAuditor;
 import com.abien.xray.business.hits.control.HitsManagement;
 import com.abien.xray.business.hits.entity.Hit;
 import com.abien.xray.business.hits.entity.Post;
-import java.util.LinkedList;
+import com.abien.xray.business.logging.boundary.XRayLogger;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.interceptor.Interceptors;
@@ -28,15 +30,16 @@ public class TopHitsResource extends TitleFilter {
     @Inject
     HitsManagement hits;
 
+    @Inject
+    XRayLogger LOG;
+
     @GET
     @Produces({MediaType.APPLICATION_XHTML_XML, MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public List<Post> totalHitsAsString(@QueryParam("max") @DefaultValue("10") int max) {
-        List<Post> mostPopularPosts = new LinkedList<>();
         List<Hit> mostPopularPostsWithoutTitle = hits.getMostPopularPosts(max);
-        mostPopularPostsWithoutTitle.stream().forEach((hit) -> {
-            mostPopularPosts.add(convert(hit));
-        });
-        return getPostsWithExistingTitle(mostPopularPosts, max);
+        LOG.log(Level.INFO, "Got hit list: " + mostPopularPostsWithoutTitle);
+        List<Post> collect = mostPopularPostsWithoutTitle.stream().map(this::convert).collect(Collectors.toList());
+        return getPostsWithExistingTitle(collect, max);
     }
 
     Post convert(Hit hit) {
