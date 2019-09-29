26261497906d27c82828c7a60e8acf9b4be5dbe3
Adam Bien <abien@adam-bien.com>
2019-09-29 19:26:48 +0200

    Migrated to Map

--- x-ray-services/src/main/java/com/abien/xray/business/hits/control/HitsCache.java
+++ x-ray-services/src/main/java/com/abien/xray/business/hits/control/HitsCache.java
@@ -3,24 +3,24 @@ package com.abien.xray.business.hits.control;
 import com.abien.xray.business.hits.entity.CacheValue;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-import javax.cache.Cache;
 
 /**
  * User: blog.adam-bien.com Date: 17.02.11 Time: 21:11
  */
 public class HitsCache {
 
-    private Cache<String, String> hits = null;
+    private Map<String, String> hits = null;
     /**
      * Left and right swapped on purpose -> decreasing order
      */
-    private final Comparator<Cache.Entry<String, String>> decreasing = (l, r) -> new Long(Long.parseLong(r.getValue())).compareTo(Long.parseLong(l.getValue()));
+    private final Comparator<Map.Entry<String, String>> decreasing = (l, r) -> new Long(Long.parseLong(r.getValue())).compareTo(Long.parseLong(l.getValue()));
 
-    public HitsCache(Cache<String, String> hits) {
+    public HitsCache(Map<String, String> hits) {
         this.hits = hits;
     }
 
@@ -46,24 +46,24 @@ public class HitsCache {
         }
     }
 
-    public Cache<String, String> getCache() {
+    public Map<String, String> getCache() {
         return hits;
     }
 
     public long getCacheSize() {
-        return StreamSupport.stream(this.hits.spliterator(), false).count();
+        return this.hits.size();
     }
 
     public List<CacheValue> getMostPopularValuesNotContaining(String excludeContaining, int maxNumber) {
-        return StreamSupport.stream(this.hits.spliterator(), true).
+        return StreamSupport.stream(this.hits.entrySet().spliterator(), true).
                 filter(f -> !f.getKey().contains(excludeContaining)).
                 sorted(decreasing).
                 map(f -> new CacheValue(f.getKey(), f.getValue())).
                 collect(Collectors.toList());
     }
 
-    public List<CacheValue> getMostPopularValues(int maxNumber, Predicate<Cache.Entry<String, String>> filter) {
-        return StreamSupport.stream(this.hits.spliterator(), true).
+    public List<CacheValue> getMostPopularValues(int maxNumber, Predicate<Map.Entry<String, String>> filter) {
+        return StreamSupport.stream(this.hits.entrySet().spliterator(), true).
                 filter(filter).
                 sorted(decreasing).
                 limit(maxNumber).
@@ -72,7 +72,7 @@ public class HitsCache {
     }
 
     public long getTotalHits() {
-        return StreamSupport.stream(this.hits.spliterator(), false).mapToLong(v -> Long.parseLong(v.getValue())).sum();
+        return StreamSupport.stream(this.hits.entrySet().spliterator(), false).mapToLong(v -> Long.parseLong(v.getValue())).sum();
     }
 
     public void clear() {
