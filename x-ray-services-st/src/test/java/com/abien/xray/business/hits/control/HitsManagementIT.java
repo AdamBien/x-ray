2e02c60959f3d3711cc3b6c1c841eb7cb62e96d9
Adam Bien <abien@adam-bien.com>
2019-10-06 17:40:00 +0200

    URI encoder used

---
+++ x-ray-services-st/src/test/java/com/abien/xray/business/hits/control/HitsManagementIT.java
@@ -0,0 +1,62 @@
+package com.abien.xray.business.hits.control;
+
+import static org.junit.Assert.assertThat;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.CoreMatchers.is;
+import com.abien.xray.business.ServerLocation;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * HitsManagementIT
+ */
+public class HitsManagementIT {
+
+    private static final String ROOT_TARGET = ServerLocation.getLocation() + "/x-ray/resources/hits";
+    private WebTarget tut;
+
+    @Before
+    public void initClient() {
+        Client client = ClientBuilder.newClient();
+        this.tut = client.target(ROOT_TARGET);
+    }
+
+    @Test
+    public void getNotExistingURI() {
+        String numberOfHits = this.tut.
+        path(URLEncoder.encode("/entry/NOTEXISTS")).
+                request(MediaType.TEXT_PLAIN).
+        get(String.class);
+        assertThat(numberOfHits, is("0"));
+    }
+
+    @Test
+    public void putAndCount() throws UnsupportedEncodingException {
+        long id = System.nanoTime();
+        String uri = URLEncoder.encode("/entry/NEW" + id,"UTF-8");
+
+        String numberOfHits = this.tut.path(uri).request(MediaType.TEXT_PLAIN).get(String.class);
+        assertThat(numberOfHits, is("0"));
+
+        Response response = this.tut.request(MediaType.TEXT_PLAIN).put(Entity.text(uri));
+        assertThat(response.getStatus(), is(204));
+        
+
+        numberOfHits = this.tut.path(uri).request(MediaType.TEXT_PLAIN).get(String.class);
+        assertThat(numberOfHits, is("1"));
+
+    }
+
+    
+}
