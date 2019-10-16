67924cd65be5fd46b3e56dbbed6ab7d6f7e59569
Adam Bien <abien@adam-bien.com>
2019-10-16 07:10:05 +0200

    superfluous modules deleted

--- satellite-ui/public_html/lib/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var satellite = angular.module('satellite', []);
-
-//var host = "http://192.168.0.42:5680/satellite/resources/grids/";
-var host = "http://localhost:8080/satellite/resources/grids/";
-
-
-
-
